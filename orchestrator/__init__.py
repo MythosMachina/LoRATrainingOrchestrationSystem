51d62225739d6730b1f130789d15ca31906b2534
AsaTyr <43162495+AsaTyr2018@users.noreply.github.com>
2025-09-09 12:02:39 +0200

    Add volume support for training pods

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -1,5 +1,5 @@
 """Orchestrator package for managing worker nodes."""
 
-from .k8s_orchestrator import KubernetesOrchestrator, WorkerSpec
+from .k8s_orchestrator import KubernetesOrchestrator, WorkerSpec, VolumeSpec
 
-__all__ = ["KubernetesOrchestrator", "WorkerSpec"]
+__all__ = ["KubernetesOrchestrator", "WorkerSpec", "VolumeSpec"]
